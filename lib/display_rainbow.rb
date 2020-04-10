baf9adecafc2386930e9974427c03924fb7c66f2
Andrew P <andrewphaviseth@gmail.com>
2020-04-10 19:46:23 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,3 +3,5 @@ def display_rainbow(colors)
   puts "R:#{colors[0]}],O:#{colors[1]}],Y:#{colors[2]}],
         G:#{colors[4]}],B:#{colors[5]}],I:#{colors[6]},V:#{colors[7]}]"
 end
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
